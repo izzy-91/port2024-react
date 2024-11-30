f7598c2f5cd10377042f0e387fbe6d3d4628a216
izzy-91 <www.izzy.xoxo@gmail.com>
2024-11-30 19:19:15 +0900

    🥺 마무리 완료

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Skill from "../components/Skill";
 import Skip from "../components/Skip";
 import Header from "../components/Header";
